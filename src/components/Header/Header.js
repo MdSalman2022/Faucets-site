d9818a59890eb3ef295f29bfeb53d63e434e422a
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-12-12 18:37:09 +0600

    responsive done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,11 +9,19 @@ import { GiTwoCoins } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import { Context } from '../../contexts/Contexts';
 import './Header.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Header = () => {
 
 
     const { currency, setCurrency } = useContext(Context)
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
 
     console.log(currency)
@@ -27,8 +35,11 @@ const Header = () => {
             <Container className='d-flex justify-content-between '>
                 <Link to="/"><Navbar.Brand ><span className='fs-3 fw-bold text-color text-decoration-none'>Faucets</span></Navbar.Brand></Link>
                 <div className='d-flex align-items-center'>
+
                     <Nav className='border'>
-                        <NavDropdown title={`${currency}`} id="collasible-nav-dropdown">
+                        <NavDropdown title={`${currency}`} id="collasible-nav-dropdown me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '50px' }}
+                            navbarScroll>
                             <NavDropdown.Item href="#action/3.1" onClick={() => currencyFunc('Arbitrum Rinkby')}>  <GiTwoCoins />  Arbitrum Rinkby</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2" onClick={() => currencyFunc('Avalanche Fuji')}> <GiTwoCoins />  Avalanche Fuji</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3" onClick={() => currencyFunc('BNB Chain TESTNET')}> <GiTwoCoins />  BNB Chain TESTNET</NavDropdown.Item>
@@ -39,11 +50,34 @@ const Header = () => {
                             <NavDropdown.Item href="#action/3.3" onClick={() => currencyFunc('Polygon Mumbai')}> <GiTwoCoins />  Polygon Mumbai</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
+
                     <Nav>
-                        <Nav.Link href="#deets"> <div className="wallet-btn"> <FaWallet /> Connect Wallet</div></Nav.Link>
+                        <Nav.Link > <div className="wallet-btn" onClick={handleShow}> <FaWallet /> <span className='d-lg-inline-block d-none' > Connect Wallet</span></div></Nav.Link>
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Connect your wallet</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <div className="row justify-content-center text-center gap-3 m-2">
+                                    <div class="col bg-light">
+                                        <img src="https://i.ibb.co/VLyWMxz/Meta-Mask-Fox-svg.png" alt="Meta-Mask-Fox-svg" border="0" className='w-75' />
+                                        <h3 className='pt-1'> MetaMask</h3>
+                                    </div>
+                                    <div class="col bg-light">
+                                        <img src="https://i.ibb.co/ZJhm3S3/social.png" alt="social" border="0" className='w-100'></img>
+                                        <h3 className='pt-3'>WalletConnect</h3>
+                                    </div>
+                                </div>
+                            </Modal.Body>
+                        </Modal>
+
+
+
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                        <NavDropdown title="Profile" id="collasible-nav-dropdown collasible-nav-dropdown me-auto my-2 my-lg-0" style={{ maxHeight: '50px' }}
+                            navbarScroll>
                             <Link to="/login"><NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item></Link>
                             <Link to="/signup"><NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item></Link>
                             <Link to="/faq"><NavDropdown.Item href="#action/3.3">FAQ</NavDropdown.Item></Link>
